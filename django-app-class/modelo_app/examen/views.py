e89fbafbb124e2004491ca7026372f0e2bbd038e
Ventthos <ferra04@live.com.mx>
2025-02-28 10:03:10 -0600

    Eventos page

--- django-app-class/modelo_app/examen/views.py
+++ django-app-class/modelo_app/examen/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from .models import Eventos
 
+def parseEventos(eventosPrev):
+    eventos = []
+
+    for evento in eventosPrev:
+        evento_dict = {
+            "nombre": evento.name,
+            "contenido": "Curabitur vehicula eros at metus interdum, sed dictum enim fringilla. Ut faucibus sem euismod dui facilisis, at sollicitudin justo pharetra.",
+            "imagen": "https://img.freepik.com/vector-gratis/patron-carnaval-brasileno-diseno-plano_23-2148811693.jpg?t=st=1740720734~exp=1740724334~hmac=46ffceba54f133084e981e411ce9e4656b98741580e180f4df2229456f17d8da&w=900",
+            "fecha_inicio": evento.fecha_inicio,
+            "fecha_fin": evento.fecha_fin,
+            "localidad": evento.localidad_id.name
+        }
+        eventos.append(evento_dict)
+    return eventos
+
 # Create your views here.
 def index(request):
 
@@ -19,24 +34,20 @@ def index(request):
 
     eventosPrev = Eventos.objects.all()
 
-    eventos = []
-
-    for evento in eventosPrev:
-        evento_dict = {
-            "nombre": evento.name,
-            "contenido": "Curabitur vehicula eros at metus interdum, sed dictum enim fringilla. Ut faucibus sem euismod dui facilisis, at sollicitudin justo pharetra.",
-            "imagen": "https://img.freepik.com/vector-gratis/patron-carnaval-brasileno-diseno-plano_23-2148811693.jpg?t=st=1740720734~exp=1740724334~hmac=46ffceba54f133084e981e411ce9e4656b98741580e180f4df2229456f17d8da&w=900",
-            "fecha_inicio": evento.fecha_inicio,
-            "fecha_fin": evento.fecha_fin,
-            "localidad": evento.localidad_id.name
-        }
-        eventos.append(evento_dict)
-    print(eventos)
+    eventos = parseEventos(eventosPrev)
     data = {
         "noticias": noticias,
-        "eventos": eventos,
-        
+        "eventos": eventos,  
     }
     
 
     return render(request, 'examen/index.html', data)
+
+
+def eventosPage(request):
+    eventosPrev = Eventos.objects.all()
+    eventos = parseEventos(eventosPrev)
+    data = {
+        "eventos": eventos, 
+    }
+    return render(request, 'examen/eventos.html', data)
