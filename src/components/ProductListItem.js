8b2bcf45d708e08c62f3cb4dc6db16756f711819
Ventthos <ferra04@live.com.mx>
2025-03-21 01:58:50 -0600

    Tarea

---
+++ src/components/ProductListItem.js
@@ -0,0 +1,16 @@
+import '../style/products.css'
+
+export default function ProductListItems(){
+    return(
+        <div className="product-list-item">
+            <div className="product-image">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzRC6TEW7daHfRIUJKbCPYkVJQjZTz2v5tIVJ18-VSKGahzUJ-ruBWAP7pTvVAvhQpQ2USJirQZuTu0XI1RG6oNg"/>
+            </div>
+            <div className="product-details">
+                <h4>Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet</h4>
+                <p>Lorem ipsum dolor sit amet</p>
+            </div>
+        </div>
+    )
+
+}
---
+++ src/page/Login.js
@@ -0,0 +1,33 @@
+import '../style/login.css'
+import MyInput from '../components/Form/MyInput';
+
+export default function Login()
+{
+    return (
+        <div className="loginBackground">
+            <div className='degradado'></div>
+            <div className='containerLoginBorder'>
+                <div className="containerLogin">
+                    
+                    <h3>Register</h3>
+                    <form className="formLogin">
+                        <div>
+                            <label htmlFor="user">Name</label>
+                            <input className='login-input' type="text" placeholder='Enter your name' name='user'  />
+                        </div>
+                        <div>
+                            <label htmlFor="email">Email</label>
+                            <input className='login-input' type="password" placeholder='Example@gmail.com' name='email'  />
+                        </div>                  
+                        <div className='login-button-container'>
+                            <button className='login-button' type='submit'>
+                                Continue
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
