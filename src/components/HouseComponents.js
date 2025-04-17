6583e98ccbfcab4cccf5682b0ee11f039933555e
Ventthos <ferra04@live.com.mx>
2025-04-17 11:38:19 -0600

    houseComponent

--- src/components/HouseComponents.js
+++ src/components/HouseComponents.js
@@ -1,20 +1,36 @@
 import { SuperHostIndicator } from "./SuperHostIndicator";
+import { FaHouseCrack, FaStar  } from "react-icons/fa6";
+import { IoPerson } from "react-icons/io5";
+import '../styles/HouseComponent.css'
 
-export function HouseComponent(){
+export function HouseComponent({name, image, description, numberBedrooms, numberGuests, price, stars}){
     return(
-        <div>
-            <div>
+        <div className="houseComponent">
+            <div className="houseComponent-Image">
                <SuperHostIndicator/>
             </div>
-            <div>
+            <div className="houseComponent-Info">
                 <h1>Nordic Retreat with Stuning Views</h1>
                 <p>Immerse yourself in the beauty of the Nordic landscape at this sleek and stylish cabin. With floor-to-ceiling windows and a spacious deck, you'll have breathtaking views of the surrounding mountains.</p>
-                <div>
-                    <div>
-
+                <div className="houseComponent-Info-Especifications">
+                    <div className="iconInfo">
+                        <FaHouseCrack className="iconInfo-Icon"/>
+                        2 BedRoom
+                    </div>
+                    <div className="iconInfo">
+                        <IoPerson className="iconInfo-Icon"/>
+                        3 Guests
+                    
                     </div>
                 </div>
                 <hr/>
+                <div className="houseComponent-Info-AditionalInfo">
+                    <p>$250/night</p>
+                    <div className="iconInfo">
+                        <FaStar className="starIconOrange"/>
+                        4.9
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,16 +1,19 @@
 import { HouseComponent } from "../components/HouseComponents";
+import '../styles/SearchPage.css';
 
 export function SearchPage(){
     return(
-        <>
+        <div className="searchPage">
             <div>
                 <p></p>
                 <input/>
             </div>
-            <main>
+            <main className="housesGrid">
+                <HouseComponent/>
+                <HouseComponent/>
                 <HouseComponent/>
             </main>
-        </>
+        </div>
     )
 
 }
