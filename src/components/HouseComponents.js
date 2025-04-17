e188783c1c5278db66a12c9dea6c43c32ab4c5f1
Ventthos <ferra04@live.com.mx>
2025-04-17 11:57:53 -0600

    Get houses

--- src/components/HouseComponents.js
+++ src/components/HouseComponents.js
@@ -3,32 +3,32 @@ import { FaHouseCrack, FaStar  } from "react-icons/fa6";
 import { IoPerson } from "react-icons/io5";
 import '../styles/HouseComponent.css'
 
-export function HouseComponent({name, image, description, numberBedrooms, numberGuests, price, stars}){
+export function HouseComponent({name, image, description, numberBedrooms, numberGuests, price, stars, superHost}){
     return(
         <div className="houseComponent">
-            <div className="houseComponent-Image">
-               <SuperHostIndicator/>
+            <div className="houseComponent-Image" style={{backgroundImage:`url(${image})`}}>
+               {superHost && <SuperHostIndicator/>}
             </div>
             <div className="houseComponent-Info">
-                <h1>Nordic Retreat with Stuning Views</h1>
-                <p>Immerse yourself in the beauty of the Nordic landscape at this sleek and stylish cabin. With floor-to-ceiling windows and a spacious deck, you'll have breathtaking views of the surrounding mountains.</p>
+                <h1>{name}</h1>
+                <p>{description}</p>
                 <div className="houseComponent-Info-Especifications">
                     <div className="iconInfo">
                         <FaHouseCrack className="iconInfo-Icon"/>
-                        2 BedRoom
+                        {numberBedrooms} BedRoom
                     </div>
                     <div className="iconInfo">
                         <IoPerson className="iconInfo-Icon"/>
-                        3 Guests
+                        {numberGuests} Guests
                     
                     </div>
                 </div>
                 <hr/>
                 <div className="houseComponent-Info-AditionalInfo">
-                    <p>$250/night</p>
+                    <p>${price}/night</p>
                     <div className="iconInfo">
                         <FaStar className="starIconOrange"/>
-                        4.9
+                        {stars}
                     </div>
                 </div>
             </div>
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,19 +1,49 @@
+import { useEffect, useState } from "react";
 import { HouseComponent } from "../components/HouseComponents";
-import '../styles/SearchPage.css';
+import "../styles/SearchPage.css";
 
-export function SearchPage(){
-    return(
-        <div className="searchPage">
-            <div>
-                <p></p>
-                <input/>
-            </div>
-            <main className="housesGrid">
-                <HouseComponent/>
-                <HouseComponent/>
-                <HouseComponent/>
-            </main>
-        </div>
-    )
+export function SearchPage() {
+  const [houses, setHouses] = useState(null);
 
+  async function getHouses() {
+    const response = await fetch(
+      "https://raw.githubusercontent.com/devchallenges-io/curriculum/refs/heads/main/4-frontend-libaries/challenges/group_1/data/property-listing-data.json"
+    );
+    const data = await response.json();
+    return data;
+  }
+
+  useEffect(() => {
+    const setHousesInState = async () => {
+      const data = await getHouses();
+      setHouses(data);
+    };
+    setHousesInState();
+  }, []);
+
+  return (
+    <div className="searchPage">
+      <div>
+        <p></p>
+        <input />
+      </div>
+      <main className="housesGrid">
+        {houses?.map((house) => {
+          return (
+            <HouseComponent
+              key={house.id}
+              name={house.title}
+              image={house.image}
+              description={house.description}
+              numberBedrooms={house.capacity.bedroom}
+              numberGuests={house.capacity.people}
+              price={house.price}
+              stars={house.rating}
+              superHost={house.superhost}
+            />
+          );
+        })}
+      </main>
+    </div>
+  );
 }
